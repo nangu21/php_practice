562ddac2501dc817db68d6999209e3f37f6a2cbe
Nangu <44922640+nangu21@users.noreply.github.com>
2020-11-06 00:08:31 +0900

    Create switch.php

---
+++ switch.php
@@ -0,0 +1,28 @@
+<?php
+
+function returnSeason($month){
+  switch($month){
+    case "December";
+    case "January";
+    case "February";
+      return "winter";
+      break;
+    case "March";
+    case "April";
+    case "May";
+      return "spring";
+      break;
+    case "June";
+    case "July";
+    case "August";
+      return "summer";
+      break;
+    case "September";
+    case "October";
+    case "November";
+      return "fall";
+      break;
+  }
+}
+
+echo returnSeason("July");
