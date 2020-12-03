3df3d0a593c44e6a4a3ed14264263791f2be78fb
Nangu <44922640+nangu21@users.noreply.github.com>
2020-12-03 21:52:39 +0900

    Add files via upload

--- 進捗管理/add_info.php
+++ 進捗管理/add_info.php
@@ -1,42 +1,5 @@
 <?php
-if(!empty($_POST['name'])){
-//参考書タイトルを取得
-//////$name = $_POST['name'];
-//今日の日付取得
-$today = time();
-//終了予定日の日付取得
-$xday = strtotime($_POST['date']);
-
-//差を計算(秒数)
-$result = $xday - $today;
-//差を日数に変換(小数点以下切り捨て)
-$interval = floor($result / (60*60*24));
-//差を週の数に変換(小数点以下切り捨て)
-$num_week = floor($interval / 7);
-
-//進捗ペース(optionタグのvalueを取得)
-///////if(isset($_POST['rest'])){
-	$rest = $_POST['rest'];
-/////}
-
-//稼働日数計算
-$active = $interval - ($rest * $num_week);
-
-//ページ数取得
-//$pages = htmlspecialchars($_POST['pages']);
-
-//進捗計算結果
-//$disp = $pages / $active;
-//残りのページ数
-$left = $_POST['pages'] - $_POST['current_page'];
-
-//進捗割合
-$racio = $_POST['current_page'] / $_POST['pages'] *100;
-
- $disp = $left / $active;
- }
- 
- //ここからjsonファイルの操作
+//ここからjsonファイルの操作
 //特殊文字の変換
 function h($str){
 	return htmlspecialchars($str, ENT_QUOTES);
@@ -56,8 +19,25 @@ foreach($rows as $key => $value){
     $max_id = $value['id'];
     }
  }
-
-
+ 
+ 
+ if (!empty($_POST['add_name'])) {
+//$_POST['add_name']が空でないとき+form送信されたとき
+//入力された name+pages+intervalで連想配列$rowを組み立てる
+    $row = array(
+      'id' => $max_id + 1,
+      'name' => $_POST['add_name'],
+      'pages' => $_POST['add_pages'],
+      'rest' => $_POST['add_rest'],
+      'pace' => $_POST['pace_result'],
+      'racio' => $_POST['racio_result']
+    );
+    //$rowを配列$rowsの最後に加える
+    //最初に加える場合はarray_unshift()
+    array_unshift($rows, $row);
+    //json形式に変換してtextbooks_info.jsonとして保存
+    file_put_contents('textbooks_info.json', json_encode($rows));
+  }
 
 ?>
 
@@ -71,31 +51,20 @@ foreach($rows as $key => $value){
 </head>
 <body>
 <div class="header">
-<h1>進捗管理アプリ</h1>
+    <div class="navbar">
+        <h1><a href="progressManage.php" style="text-decoration: none;">進捗管理アプリ</a></h1><br>
+    </div>
 </div>
-<!--
-<p><?= $today ?></p>
-<p><?= htmlspecialchars($xday) ?></p>
-<p><?= htmlspecialchars($name) ?></p>
-<p><?= "日数残り" .$interval ."日(". $num_week ."週間)";  ?></p>
-<p><?= "週休" .$rest; ?></p>
-<p><?= "稼働日数" . $active; ?></p>
-<p><?= "結果：1日" . $disp. "ページ"; ?></p>
--->
-<h2><? printf("結果：1日  %.2f ページ", $disp); ?></h2>
-
-<form method="post" action="add_info.php">
-<p>『<?= $_POST['name'] ?>』を進捗グラフに追加しますか？</p>
-<!--あとでhiddenにする-->
-<input type="hidden" name="add_name" value="<?= $_POST['name'] ?>">
-<input type="hidden" name="add_pages" value="<?= $_POST['pages'] ?>">
-<input type="hidden" name="add_date" value="<?= $_POST['date'] ?>">
-<input type="hidden" name="add_rest" value="<?= $_POST['rest'] ?>">
-<input type="hidden" name="id" value="<?php echo $row['id'] ?>">
-<input type="hidden" name="pace_result" value="<? printf("%.1f", $disp) ?>">
-<input type="hidden" name="racio_result" value="<? printf("%d", $racio) ?>">
-<input type="submit" name="add" value="追加">
-</form>
+<!--全ての投稿を表示する-->
+<div class="wrap">
+<?php foreach($rows as $row): ?>
+    <div class="item">
+        <strong><?php echo h($row['name']) ?></strong>
+        <p><?php echo h("1日".$row['pace']). "ページ" ?></p>
+        <p>進捗度： <?= $row['racio'] ?>％</p>
+    </div>
+<?php endforeach ?>
+</div>
 
 <!--描画領域 -->
 <canvas id="canvas"></canvas>
@@ -109,12 +78,12 @@ foreach($rows as $key => $value){
             	 <?php echo "'". h($row['name']). "'" . "," ?>
               <?php endforeach ?>
             ],     //ラベル名
-            datasets: [{                       //データ設定
+            datasets: [{                      //データ設定
                 data: [
                 <?php foreach($rows as $row): ?>
                     <?php echo h($row['racio']). ","?>
                 <?php endforeach ?>
-                ],          //データ内容
+            ],          //データ内容
                 backgroundColor: ['#F97F51', '#25CCF7', '#D6A2E8', '#58B19F', '#FC427B', '#EAB543']   //背景色
             }]
         },
@@ -147,7 +116,7 @@ foreach($rows as $key => $value){
                     
                     scaleLabel: {                 //軸ラベル設定
                        display: true,             //表示設定
-                       //labelString: '横軸',  //ラベル
+                    //  labelString: '横軸',  //ラベル
                        fontSize: 18               //フォントサイズ
                     },
                     ticks: {		//最大値最小値設定
