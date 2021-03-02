6363d32101b828bffc4dbec5991f6bcf247252cf
Nangu <44922640+nangu21@users.noreply.github.com>
2021-03-02 13:37:37 +0900

    ターミナルにおける標準入力
    「0」入力で1行、「1」入力で複数行の標準入力。「end」入力で入力を完了する。

--- 標準出力.php
+++ 標準出力.php
@@ -1,6 +1,6 @@
 <?php 
 //標準入力
-    echo '1行だけ入力する場合は0を、複数行入力する場合は1を入力してね: ';
+    echo '1行だけ入力する場合は「0」を、複数行入力する場合は「1」を入力してね: ';
     $num = trim(fgets(STDIN));
     if($num == 1){
         echo '内容を入力してね: ';
@@ -8,7 +8,7 @@
         echo '受け取りました: ';
         var_dump($stdin);
     }else if($num == 0){
-        echo '内容を入力してね。終わったらendって入力してね: ';
+        echo '内容を入力してね。終わったら「end」と入力してね: ';
         while($stdin = trim(fgets(STDIN))){
             if($stdin !== 'end'){
                 $lines[] = $stdin;
